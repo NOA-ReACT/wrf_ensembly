f08437828dcd5b99e3412dd97fc18d76c1630250
Thanasis Georgiou <ageorgiou@noa.gr>
2023-12-04 13:23:08 +0200

    Fix build system

--- wrf_ensembly/__init__.py
+++ wrf_ensembly/__init__.py
@@ -1,10 +1,26 @@
+import typer
+
 from wrf_ensembly import (
+    commands,
     config,
     console,
     cycling,
+    member_info,
     namelist,
     templates,
     utils,
     wrf,
-    member_info,
 )
+
+
+def main():
+    """CLI entry point for the application"""
+
+    app = typer.Typer()
+    app.add_typer(commands.experiment.app, name="experiment")
+    app.add_typer(commands.preprocess.app, name="preprocess")
+    app.add_typer(commands.ensemble.app, name="ensemble")
+    app.add_typer(commands.slurm.app, name="slurm")
+    app.add_typer(commands.observations.app, name="observations")
+
+    app()
