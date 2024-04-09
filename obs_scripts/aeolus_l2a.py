dbc2172ce9edbad570eb9823316f3e4454345255
Thanasis Georgiou <ageorgiou@noa.gr>
2024-04-09 11:14:31 +0300

    Add obs_script for AEOLUS_L2A
    and small fix in AEOLUS L2B script :)

--- obs_scripts/aeolus_l2a.py
+++ obs_scripts/aeolus_l2a.py
@@ -10,29 +10,34 @@ import tomli_w
 @click.command()
 @click.argument("dir_path", type=click.Path(exists=True, path_type=Path))
 @click.argument("output_path", type=click.Path(path_type=Path))
-def create_aeolus_l2b_obsgroup(dir_path: Path, output_path: Path):
+def create_aeolus_l2a_obsgroup(dir_path: Path, output_path: Path):
     """
     Creates the observation group file for the given directory of AEOLUS L2B DBL files
     """
 
-    prefix = "AE_OPER_ALD_U_N_2B_"
-    date_regex = re.compile(r"(\d{8}T\d{6})_(\d{8}T\d{6})")
+    prefix = "AE_OPER_ALD_U_N_2A_"
+    date_regex = re.compile(r"(\d{8}T\d{9})_(\d{9})")
 
     files = []
     for dbl in chain(dir_path.glob("*.dbl"), dir_path.glob("*.DBL")):
         click.echo(f"Processing {dbl}")
 
-        # Sample file name: AE_OPER_ALD_U_N_2B_20210901T003156_20210901T015956_0001.DBL
+        # Sample file name: AE_OPER_ALD_U_N_2A_20210903T164708024_005532006_017567_0001.DBL
+        # From docs: yyyymmddThhmmsszzz_uuuuuuuuu_oooooo_vvvv
+        # yyyymmddThhmmsszzz: start time of the measurement
+        # uuuuuuuuu: duration of the measurement in milliseconds
+        # oooooo: orbit number
+        # vvvv: version number
         name = dbl.name
         if name[: len(prefix)] != prefix:
             click.echo(f"File prefix is not {prefix}!")
             continue
 
         start, end = date_regex.findall(name)[0]
-        start = dt.datetime.strptime(start, "%Y%m%dT%H%M%S").replace(
+        start = dt.datetime.strptime(start[:-3], "%Y%m%dT%H%M%S").replace(
             tzinfo=dt.timezone.utc
         )
-        end = dt.datetime.strptime(end, "%Y%m%dT%H%M%S").replace(tzinfo=dt.timezone.utc)
+        end = start + dt.timedelta(milliseconds=int(end))
 
         files.append({"start_date": start, "end_date": end, "path": str(dbl.resolve())})
 
@@ -44,8 +49,8 @@ def create_aeolus_l2b_obsgroup(dir_path: Path, output_path: Path):
     files = sorted(files, key=lambda f: f["start_date"])
 
     obsgroup = {
-        "kind": "AEOLUS_L2B",
-        "converter": "/path/to/convert_aeolus_l2b",
+        "kind": "AEOLUS_L2A",
+        "converter": "/path/to/convert_aeolus_l2a",
         "files": files,
     }
     with open(output_path, "wb") as f:
@@ -54,4 +59,4 @@ def create_aeolus_l2b_obsgroup(dir_path: Path, output_path: Path):
 
 
 if __name__ == "__main__":
-    create_aeolus_l2b_obsgroup()
+    create_aeolus_l2a_obsgroup()
