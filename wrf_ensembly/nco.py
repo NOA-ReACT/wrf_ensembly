2d692e03e17a6912f3f4aedc12556d01d909001e
Thanasis Georgiou <ageorgiou@noa.gr>
2025-04-10 17:45:21 +0300

    Remove unused NCO functions

--- wrf_ensembly/nco.py
+++ wrf_ensembly/nco.py
@@ -3,46 +3,6 @@ from pathlib import Path
 from wrf_ensembly.external import ExternalProcess
 
 
-def average(input_files: list[Path], output_file: Path) -> ExternalProcess:
-    """
-    Average a set of netCDF files using NCO
-    You need to execute the returned object to run the command using external.run()
-    """
-
-    output_file.parent.mkdir(parents=True, exist_ok=True)
-
-    return ExternalProcess(
-        [
-            "nces",
-            "-4",
-            "-O",
-            *[str(x.resolve()) for x in input_files],
-            str(output_file.resolve()),
-        ]
-    )
-
-
-def standard_deviation(input_files: list[Path], output_file: Path) -> ExternalProcess:
-    """
-    Calculate the standard deviation of a set of netCDF files using NCO
-    You need to execute the returned object to run the command using external.run()
-    """
-
-    output_file.parent.mkdir(parents=True, exist_ok=True)
-
-    return ExternalProcess(
-        [
-            "nces",
-            "-4",
-            "-O",
-            "-y",
-            "rmssdn",
-            *[str(x.resolve()) for x in input_files],
-            str(output_file.resolve()),
-        ]
-    )
-
-
 def concatenate(
     input_files: list[Path], output_file: Path, args: list[str] = []
 ) -> ExternalProcess:
