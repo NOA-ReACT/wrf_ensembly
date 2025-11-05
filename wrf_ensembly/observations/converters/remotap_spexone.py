bad67e6c8c83806151ab6e0d3eaf1dab37c7adde
Thanasis Georgiou <ageorgiou@noa.gr>
2025-11-05 16:45:56 +0200

    RemoTAP-SPEXone converter: Filter with IMERG before coarsen

--- wrf_ensembly/observations/converters/remotap_spexone.py
+++ wrf_ensembly/observations/converters/remotap_spexone.py
@@ -11,9 +11,47 @@ import xarray as xr
 from wrf_ensembly.observations import io as obs_io
 
 
-def parse_spex_date(
-    utc_date: np.ndarray, fraction_of_day: np.ndarray
-) -> pd.DatetimeIndex:
+def verify_product_name(path: Path) -> str:
+    """Verify that the product name matches the expected RemoTAP-spexone pattern. Raises ValueError if not."""
+    with xr.open_dataset(path) as root_ds:
+        product_name = root_ds.attrs.get("product_name", "unknown")
+    matches = re.match(r"PACE_SPEXONE.*AER_(?:LAND|OCEAN)_REMOTAP\.nc", product_name)
+    if not matches:
+        raise ValueError(
+            f"File {path} is not a valid SPEX file. "
+            r"Expected product name to match 'PACE_SPEXONE.*AER_(?:LAND|OCEAN)_REMOTAP\.nc', "
+            f"but got '{product_name}'"
+        )
+    return product_name
+
+
+def mask_wrong_retrievals(
+    geolocation: xr.Dataset, imerg: xr.Dataset, is_ocean_retrieval: bool
+) -> xr.DataArray:
+    """
+    The RemoTAP-SPEXone files are either retrieved using the land or the ocean algorithm.
+    This function uses IMERG landseamask to generate the mask that removes the points that do not correspond
+    to the retrieval type. So if the retrieval is for ocean, all land points are masked out, and vice versa.
+
+    Args:
+        geolocation: Geolocation dataset from RemoTAP-spexone file.
+        imerg: IMERG dataset used for masking.
+        is_ocean_retrieval: Flag indicating if the retrieval is for ocean.
+
+    Returns:
+        The mask DataArray where True indicates valid points and False invalid points.
+    """
+
+    imerg_interp = imerg["landseamask"].interp(
+        lat=geolocation["latitude"], lon=geolocation["longitude"], method="linear"
+    )
+    is_ocean = imerg_interp > 60
+    mask = is_ocean if is_ocean_retrieval else ~is_ocean
+
+    return mask
+
+
+def parse_spex_date(utc_date: np.ndarray, fraction_of_day: np.ndarray) -> np.ndarray:
     """
     SPEX dates are provided in a UTC date as an integer and a float representing the fraction of day.
     This function converts them to a pandas timestamp.
@@ -45,14 +83,17 @@ def get_index_tuples(arr):
 
 
 def convert_remotap_spexone(
-    path: Path,
+    ds_geolocation: xr.Dataset,
+    ds_geophysical: xr.Dataset,
+    original_filename: str,
     across_track_bin_size: int | None = None,
     along_track_bin_size: int | None = None,
 ) -> None | pd.DataFrame:
     """Convert a RemoTAP-spexone file to WRF-Ensembly Observation format.
 
     Args:
-        path: The path to the RemoTAP-spexone netCDF file.
+        ds_geolocation: xarray Dataset containing geolocation data.
+        ds_geophysical: xarray Dataset containing geophysical data.
         across_track_bin_size: Number of across-track bins to aggregate into one using mean.
         along_track_bin_size: Number of along-track bins to aggregate into one using mean.
 
@@ -61,7 +102,6 @@ def convert_remotap_spexone(
     """
 
     # geolocation arrays & date parsing
-    ds_geolocation = xr.open_dataset(path, group="geolocation_data")
     ds_geolocation["timestamp"] = (
         ("bins_along_track", "bins_across_track"),
         parse_spex_date(
@@ -80,7 +120,10 @@ def convert_remotap_spexone(
             ds_geolocation.sizes["bins_across_track"],
             ds_geolocation.sizes["bins_along_track"],
         )
+
         ds_geolocation = ds_geolocation.coarsen(**bin_sizes, boundary="trim").mean()  # type: ignore
+        ds_geophysical = ds_geophysical.coarsen(**bin_sizes, boundary="trim").mean()  # type: ignore
+
         new_size = (
             ds_geolocation.sizes["bins_across_track"],
             ds_geolocation.sizes["bins_along_track"],
@@ -95,12 +138,13 @@ def convert_remotap_spexone(
     timestamp = ds_geolocation["timestamp"].values.flatten()
 
     # AOD & uncertainty
-    ds_geophysical = xr.open_dataset(path, group="geophysical_data")
-    if bin_sizes:
-        ds_geophysical = ds_geophysical.coarsen(**bin_sizes, boundary="trim").mean()  # type: ignore
-
     aod550 = ds_geophysical["aot550"].to_numpy().flatten()
     aod550_uncertainty = ds_geophysical["aot550_uncertainty"].to_numpy().flatten()
+
+    # QC Flag: invalid bins are NaN
+    valid_mask = ~np.isnan(aod550) & ~np.isnan(aod550_uncertainty)
+    if not np.any(valid_mask):
+        return None
 
     # We need to preserve the original indices of the valid data points
     indices = get_index_tuples(ds_geophysical["aot550"])
@@ -119,9 +163,9 @@ def convert_remotap_spexone(
             "z_type": "columnar",
             "value": aod550,
             "value_uncertainty": aod550_uncertainty,
-            "qc_flag": 0,
+            "qc_flag": valid_mask,
             # "orig_coords": ...,
-            "orig_filename": path.name,
+            "orig_filename": original_filename,
             "metadata": pd.NA,
         }
     )
@@ -182,48 +226,33 @@ def remotap_spexone(
     print(f"Converting RemoTAP-SPEX file: {input_path}")
     print(f"Output path: {output_path}")
 
-    # Sanity check product mane
-    with xr.open_dataset(input_path) as root_ds:
-        product_name = root_ds.attrs.get("product_name", "unknown")
-    if not re.match(r"PACE_SPEXONE.*AER_(?:LAND|OCEAN)_REMOTAP\.nc", product_name):
-        raise ValueError(
-            f"File {input_path} is not a valid SPEX file. "
-            r"Expected product name to match 'PACE_SPEXONE.*AER_(?:LAND|OCEAN)_REMOTAP\.nc', "
-            f"but got '{product_name}'"
-        )
+    spex_geoloc = xr.open_dataset(input_path, group="geolocation_data")
+    spex_geophys = xr.open_dataset(input_path, group="geophysical_data")
 
+    product_name = verify_product_name(input_path)
+
+    # If `imerg_path` is given, load IMERG and prepare mask
     if imerg_path is not None:
         imerg = xr.open_dataset(imerg_path)
-        imerg["is_ocean"] = imerg["landseamask"] > 60
         is_ocean_retrieval = "OCEAN" in product_name
+
+        mask = mask_wrong_retrievals(spex_geoloc, imerg, is_ocean_retrieval)
+        num_invalid = (~mask).sum().item()
         print(
-            f"Using IMERG file {imerg_path} to mark {'ocean' if is_ocean_retrieval else 'land'} points as invalid"
+            f"Using IMERG landseamask from {imerg_path} to mark {num_invalid} points as invalid"
         )
 
     # Convert the data
     converted_df = convert_remotap_spexone(
-        input_path,
+        spex_geoloc,
+        spex_geophys,
+        original_filename=input_path.name,
         across_track_bin_size=across_track_bin_size,
         along_track_bin_size=along_track_bin_size,
     )
     if converted_df is None or converted_df.empty:
         print("No observations found in the input file, aborting")
         return
-
-    # If imerg_path is given, mark ocean/land points as invalid
-    if imerg_path is not None:
-        is_ocean = converted_df.apply(
-            lambda row: imerg["is_ocean"]
-            .sel(lat=row["latitude"], lon=row["longitude"], method="nearest")
-            .item(),
-            axis=1,
-        )
-        if is_ocean_retrieval:
-            converted_df.loc[~is_ocean, "qc_flag"] = 1
-            print(f"Marked {sum(~is_ocean)} land points as invalid")
-        else:
-            converted_df.loc[is_ocean, "qc_flag"] = 1
-            print(f"Marked {sum(is_ocean)} ocean points as invalid")
 
     # Save to output path as parquet
     obs_io.write_obs(converted_df, output_path)
