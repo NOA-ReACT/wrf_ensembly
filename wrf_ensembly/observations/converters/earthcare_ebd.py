be688eb53cf7f7f5f2e1f066174f6fb9f15c8983
Thanasis Georgiou <ageorgiou@noa.gr>
2025-11-05 16:48:11 +0200

    EarthCARE EBD ATL converter: Add vertican and horizontal binning

--- wrf_ensembly/observations/converters/earthcare_ebd.py
+++ wrf_ensembly/observations/converters/earthcare_ebd.py
@@ -3,9 +3,9 @@
 from pathlib import Path
 
 import click
+import numpy as np
 import pandas as pd
 import xarray as xr
-import numpy as np
 
 from wrf_ensembly.observations import io as obs_io
 
@@ -25,11 +25,19 @@ def get_index_tuples(arr):
     return [np.unravel_index(i, arr.shape) for i in flat_indices]
 
 
-def convert_earthcare_ebd(input_path: Path) -> pd.DataFrame:
+def convert_earthcare_ebd(
+    input_path: Path,
+    vertical_bins: list[int] | None = None,
+    horizontal_bin_size: int | None = None,
+    filter_before_binning: bool = False,
+) -> pd.DataFrame:
     """Convert EarthCARE ATLID EBD file to WRF-Ensembly Observation format.
 
     Args:
         input_path: Path to the EarthCARE ATLID EBD netCDF file.
+        vertical_bin_size: Size of vertical bins for aggregation, group by this many height bins and use mean.
+        horizontal_bin_size: Size of horizontal bins for aggregation, group by this many profiles and use mean.
+        filter_before_binning: If True, apply QC filtering before binning.
     Returns:
         A pandas DataFrame in WRF-Ensembly Observation format (correct columns etc).
     """
@@ -37,7 +45,62 @@ def convert_earthcare_ebd(input_path: Path) -> pd.DataFrame:
     ec = xr.open_dataset(input_path, group="ScienceData")
     ec = ec.set_coords(["time", "latitude", "longitude", "height"])
 
-    # Flatten the data arrays
+    ec = ec[
+        [
+            "particle_extinction_coefficient_355nm_low_resolution",
+            "particle_extinction_coefficient_355nm_low_resolution_error",
+            "simple_classification",
+        ]
+    ]
+    # Prepare qc flag, 1 = valid, 0 = invalid:
+    # 1) No NaN extinction
+    # 2) Only aerosol (3) or clear air (0)
+    # 3) Extinction in [0, 0.0003]
+    qc_flag = ~ec["particle_extinction_coefficient_355nm_low_resolution"].isnull()
+    qc_flag &= (ec["simple_classification"] == 0) | (ec["simple_classification"] == 3)
+    qc_flag &= (ec["particle_extinction_coefficient_355nm_low_resolution"] >= 0) & (
+        ec["particle_extinction_coefficient_355nm_low_resolution"] < 0.0003
+    )
+
+    # Filter data before binning if requested
+    if filter_before_binning:
+        print("Applying QC filtering before binning.")
+        ec = ec.where(qc_flag, drop=False)
+
+    # Assign qc_flag back to dataset so it takes up the correct shape after possible binning
+    ec["qc_flag"] = qc_flag
+
+    # Apply binning if requested
+    if vertical_bins is not None:
+        # We can't use groupby_bins because height is 2D in the file. So we will do it manually for each vertical profile.
+        ec_downsampled = []
+        for i in range(ec.sizes["along_track"]):
+            ec_profile = ec.isel(along_track=i)
+            ec_binned = (
+                ec_profile.groupby_bins("height", vertical_bins)
+                .mean(skipna=True)
+                .rename_dims({"height_bins": "height"})
+            )
+            ec_downsampled.append(ec_binned)
+        ec = xr.concat(ec_downsampled, dim="along_track")
+
+        # For each height bin, compute the mid point, and assign as the new height coordinate
+        # Remember that this is an array of pd.Interval objects
+        bins = ec["height_bins"].values
+        bin_mids = np.array([interval.mid for interval in bins])
+
+        # The original height coordinate is 2D (along_track, height), so we need to broadcast the bin mids accordingly
+        bin_mids = np.tile(bin_mids, (ec.sizes["along_track"], 1))
+
+        # Assign the new height coordinate
+        ec = ec.drop_vars("height_bins")
+        ec["height"] = (("along_track", "height"), bin_mids)
+
+    if horizontal_bin_size is not None:
+        print(f"Applying horizontal binning with size {horizontal_bin_size}.")
+        ec = ec.coarsen(along_track=horizontal_bin_size, boundary="trim").mean()
+
+    # Flatten the data arrays, extract from `ec` again after possible binning and filtering
     particle_extinction = ec[
         "particle_extinction_coefficient_355nm_low_resolution"
     ].to_numpy()
@@ -49,6 +112,7 @@ def convert_earthcare_ebd(input_path: Path) -> pd.DataFrame:
     )
     height = ec["height"].to_numpy().flatten()
     simple_classification = ec["simple_classification"].to_numpy().flatten()
+    qc_flag = ec["qc_flag"].to_numpy().flatten()
 
     # The coordinates are 1D arrays that need to be broadcast to the shape of the data
     # The data shape is (time, height), so we need to repeat the coordinates accordingly
@@ -73,13 +137,6 @@ def convert_earthcare_ebd(input_path: Path) -> pd.DataFrame:
         .flatten()
     )
 
-    # Mark as invalid (qc=0): no extinction, not aerosol and not clear air
-    qc_flag = ~np.isnan(value)
-    qc_flag &= (simple_classification == 0) | (simple_classification == 3)
-
-    # Any extinction above 0.0003 is rejected as a possible cloud
-    qc_flag &= value < 0.0003
-
     # Preserve the original indices
     indices = get_index_tuples(particle_extinction)
     coord_names = ec["particle_extinction_coefficient_355nm_low_resolution"].dims
@@ -89,6 +146,17 @@ def convert_earthcare_ebd(input_path: Path) -> pd.DataFrame:
     # This is a bit arbitrary, but we need something reasonable
     # TODO Better uncertainty estimation
     value_uncertainty = np.where(simple_classification == 3, 0.2 * value, 0.05 * value)
+
+    # Shapes of everything
+    shapes = {
+        "time": time.shape,
+        "latitude": latitude.shape,
+        "longitude": longitude.shape,
+        "height": height.shape,
+        "value": value.shape,
+        "value_uncertainty": value_uncertainty.shape,
+        "qc_flag": qc_flag.shape,
+    }
 
     # Create dataframe in WRF-Ensembly Observation format
     df = pd.DataFrame(
@@ -120,15 +188,42 @@ def convert_earthcare_ebd(input_path: Path) -> pd.DataFrame:
 @click.command()
 @click.argument("input_path", type=click.Path(exists=True, path_type=Path))
 @click.argument("output_path", type=click.Path(path_type=Path))
-def earthcare_atl_ebd(input_path: Path, output_path: Path):
-    """Convert EarthCARE ATLID EBD file to WRF-Ensembly Observation format.
+@click.option(
+    "--vertical-bins",
+    type=str,
+    help="Which vertical bins to use for aggregation, in meters, comma-separated.",
+)
+@click.option(
+    "--horizontal-bin-size",
+    type=int,
+    help="How many horizontal bins/profiles to aggregate.",
+)
+@click.option("--filter-before-binning", is_flag=True, help="Filter before binning.")
+def earthcare_atl_ebd(
+    input_path: Path,
+    output_path: Path,
+    vertical_bins: str | None = None,
+    horizontal_bin_size: int | None = None,
+    filter_before_binning: bool = False,
+):
+    """
+    Convert EarthCARE ATLID EBD file to WRF-Ensembly Observation format.
+    If you specify either (or both) of the bin sizes, the data will be aggregated accordingly, using `mean` as the aggregation function, ignoring NaNs.
+    If you do decide to downsample, it is recommended to also use the `--filter-before-binning` flag to apply QC filtering before the binning operation.
 
     Args:
         input_path: Path to the EarthCARE ATLID EBD netCDF file.
         output_path: Path to save the converted WRF-Ensembly Observation CSV file.
     """
 
-    df = convert_earthcare_ebd(input_path)
+    vertical_bins_parsed: list[int] | None = None
+    if vertical_bins is not None:
+        vertical_bins_parsed = [int(v) for v in vertical_bins.split(",")]
+        print(f"Using vertical bins: {vertical_bins_parsed}")
+
+    df = convert_earthcare_ebd(
+        input_path, vertical_bins_parsed, horizontal_bin_size, filter_before_binning
+    )
 
     if output_path.is_dir():
         output_path = output_path / f"{input_path.stem}.parquet"
