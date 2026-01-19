5b541c0b4ab9d164230fd21dcf116b7096676523
Thanasis Georgiou <ageorgiou@noa.gr>
2026-01-19 15:57:37 +0200

    Remove import of deleted data class

--- wrf_ensembly/experiment/__init__.py
+++ wrf_ensembly/experiment/__init__.py
@@ -1,5 +1,5 @@
 from .database import ExperimentDatabase
-from .dataclasses import ExperimentStatus, MemberStatus, RuntimeStatistics
+from .dataclasses import MemberStatus, RuntimeStatistics
 from .experiment import Experiment
 from .paths import ExperimentPaths
 from .state_machine import CycleState, ExperimentStateMachine, StateTransition
