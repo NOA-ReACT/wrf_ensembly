a35fbdabdfd29c478b416bc441baf0b6958694d7
Thanasis Georgiou <ageorgiou@noa.gr>
2026-01-19 15:43:57 +0200

    Use lazy-loading for the main CLI

--- wrf_ensembly/cli.py
+++ wrf_ensembly/cli.py
@@ -2,18 +2,47 @@ import pathlib
 
 import click
 
-from wrf_ensembly.commands.ensemble import ensemble_cli
-from wrf_ensembly.commands.experiment import experiment_cli
-from wrf_ensembly.commands.obs_sequence import observation_sequence_cli
-from wrf_ensembly.commands.observations import observations_cli
-from wrf_ensembly.commands.postprocess import postprocess_cli
-from wrf_ensembly.commands.preprocess import preprocess_cli
-from wrf_ensembly.commands.slurm import slurm_cli
-from wrf_ensembly.commands.status import status_cli
-from wrf_ensembly.commands.validation import validation_cli
+
+class LazyGroup(click.Group):
+    """
+    A Click group that lazy-loads subcommands
+    Brings a small improvement in start-up time
+    More info:
+    https://click.palletsprojects.com/en/stable/complex/#lazily-loading-subcommands
+    """
+
+    def __init__(self, *args, lazy_subcommands=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.lazy_subcommands = lazy_subcommands or {}
+
+    def list_commands(self, ctx):
+        return sorted(self.lazy_subcommands.keys())
+
+    def get_command(self, ctx, cmd_name):
+        if cmd_name in self.lazy_subcommands:
+            module_path, attr = self.lazy_subcommands[cmd_name]
+            mod = __import__(module_path, fromlist=[attr])
+            return getattr(mod, attr)
+        return None
 
 
-@click.group()
+@click.group(
+    cls=LazyGroup,
+    lazy_subcommands={
+        "experiment": ("wrf_ensembly.commands.experiment", "experiment_cli"),
+        "preprocess": ("wrf_ensembly.commands.preprocess", "preprocess_cli"),
+        "ensemble": ("wrf_ensembly.commands.ensemble", "ensemble_cli"),
+        "obs-sequence": (
+            "wrf_ensembly.commands.obs_sequence",
+            "observation_sequence_cli",
+        ),
+        "slurm": ("wrf_ensembly.commands.slurm", "slurm_cli"),
+        "postprocess": ("wrf_ensembly.commands.postprocess", "postprocess_cli"),
+        "status": ("wrf_ensembly.commands.status", "status_cli"),
+        "observations": ("wrf_ensembly.commands.observations", "observations_cli"),
+        "validation": ("wrf_ensembly.commands.validation", "validation_cli"),
+    },
+)
 @click.argument(
     "experiment_path",
     required=True,
@@ -24,16 +53,6 @@ def cli(ctx, experiment_path: str):
     ctx.ensure_object(dict)
     ctx.obj["experiment_path"] = experiment_path
 
-
-cli.add_command(experiment_cli)
-cli.add_command(preprocess_cli)
-cli.add_command(ensemble_cli)
-cli.add_command(observation_sequence_cli)
-cli.add_command(slurm_cli)
-cli.add_command(postprocess_cli)
-cli.add_command(status_cli)
-cli.add_command(observations_cli)
-cli.add_command(validation_cli)
 
 if __name__ == "__main__":
     cli()
