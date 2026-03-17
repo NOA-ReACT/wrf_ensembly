154168b2058afd91b32f6ad5f81c8f12c5f23b58
Thanasis Georgiou <ageorgiou@noa.gr>
2026-03-17 11:57:08 +0200

    Add another possible error when importing a bad build of coda

--- wrf_ensembly/observations/converters/__init__.py
+++ wrf_ensembly/observations/converters/__init__.py
@@ -1,10 +1,10 @@
 """Observation format converters for WRF-Ensembly."""
 
 from .aeronet import aeronet as aeronet_cli
-from .remotap_spexone import remotap_spexone as remotap_spexone_cli
 from .earthcare_ebd import earthcare_atl_ebd as earthcare_ebd_cli
-from .viirs import viirs as viirs_cli
 from .modis import modis as modis_cli
+from .remotap_spexone import remotap_spexone as remotap_spexone_cli
+from .viirs import viirs as viirs_cli
 
 # Try to import AEOLUS converters (requires optional 'coda' dependency)
 # We need to check if the module can be imported without triggering the coda import
@@ -14,15 +14,15 @@ HAS_AEOLUS_CONVERTERS = False
 
 try:
     # This will only succeed if coda can be loaded
-    from .aeolus_l2b import HAS_CODA as has_coda_l2b
     from .aeolus_l2a import HAS_CODA as has_coda_l2a
+    from .aeolus_l2b import HAS_CODA as has_coda_l2b
 
     if has_coda_l2b and has_coda_l2a:
-        from .aeolus_l2b import aeolus_l2b as aeolus_l2b_cli
         from .aeolus_l2a import aeolus_l2a as aeolus_l2a_cli
+        from .aeolus_l2b import aeolus_l2b as aeolus_l2b_cli
 
         HAS_AEOLUS_CONVERTERS = True
-except (ImportError, OSError):
+except (ImportError, OSError, TypeError):
     # Either the module can't be imported or coda can't be loaded
     pass
 
